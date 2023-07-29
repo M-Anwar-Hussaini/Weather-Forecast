87b354a4ec72cd645976d86586582ce0c94b7efb
Anwar <anwarhussaini160@gmail.com>
2023-07-29 14:02:27 +0430

    Apply functionality for waiting

--- src/components/ProvinceList.jsx
+++ src/components/ProvinceList.jsx
@@ -1,14 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import Loading from 'react-loading';
 import Province from './Province';
 import { getApiData } from '../redux/slice';
 
 export default function ProvinceList() {
-  const { provinces, isLoading, hasError } = useSelector((store) => store.provinces);
+  const { provinces, isLoading } = useSelector((store) => store.provinces);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getApiData());
   }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="d-flex flex-column align-items-center justify-content-center gap3 p-3 bgcolor-dark">
+        <Loading type="spokes" />
+        <span>Please wait...</span>
+      </div>
+    );
+  }
+
   return (
     <div>
       {provinces.map((pro, i) => (
@@ -19,6 +30,7 @@ export default function ProvinceList() {
           temprature={pro.temprature}
           key={pro.province}
           className={i % 2 === 1 ? 'bgcolor-normal' : 'bgcolor-dark'}
+          description={pro.description}
         />
       ))}
     </div>

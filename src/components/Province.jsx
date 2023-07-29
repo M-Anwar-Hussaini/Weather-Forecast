f583fed4635d640500da11dcf7ff0b4e9323b39e
Anwar <anwarhussaini160@gmail.com>
2023-07-29 14:01:51 +0430

    Clean the code for province component

--- src/components/Province.jsx
+++ src/components/Province.jsx
@@ -1,31 +1,34 @@
 import WeatherIcon from 'react-open-weather-icons';
 import Type from 'prop-types';
-import ReactLoading from 'react-loading';
+import { FaLocationDot, FaTemperatureArrowUp } from 'react-icons/fa6';
 
 export default function Province({
-  province, icon, temprature, isLoading, className,
+  province, icon, temprature, className, description,
 }) {
   return (
-    <figure className={`p-3 m-0 ${className}`} id="province">
-      {
-          isLoading ? (
-            <div className="d-flex align-items-center justify-content-center">
-              <ReactLoading type="spokes" />
-            </div>
-          )
-            : (
-              <div className="row align-items-center justify-content-between">
+    <figure className={`px-1 py-3 m-0 ${className}`} id="province">
+      <div className="row align-items-center justify-content-between">
 
-                <span className="col-5 fs-5 fw-bold">{province}</span>
-                <WeatherIcon name={icon} className="weather-icon col-2" />
-                <span className="col-5">
-                  Temprature:&nbsp;
-                  {temprature}
-                  &deg;C
-                </span>
-              </div>
-            )
-        }
+        <div className="col-5">
+          <div className="d-flex gap-2 align-items-center">
+            <FaLocationDot fontSize={24} />
+            <span className="fs-5 fw-bold">{province}</span>
+          </div>
+        </div>
+
+        <div className="col-3 d-flex flex-column align-items-center">
+          <WeatherIcon name={icon} className="weather-icon" />
+          <span>{description}</span>
+        </div>
+
+        <div className="col-4 d-flex align-items-center gap-2">
+          <FaTemperatureArrowUp fontSize={24} />
+          <span>
+            {temprature}
+            &deg;C
+          </span>
+        </div>
+      </div>
     </figure>
   );
 }
@@ -34,6 +37,6 @@ Province.propTypes = {
   province: Type.string.isRequired,
   icon: Type.string.isRequired,
   temprature: Type.number.isRequired,
-  isLoading: Type.bool.isRequired,
-  className: Type.bool.isRequired,
+  className: Type.string.isRequired,
+  description: Type.string.isRequired,
 };

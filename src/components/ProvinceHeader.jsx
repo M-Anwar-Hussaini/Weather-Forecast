eb01261ff0a32f4d5b0da603ea9a426bf424124b
Anwar <anwarhussaini160@gmail.com>
2023-07-31 16:28:55 +0430

    Enclose carousel inside a wrapper container

--- src/components/ProvinceHeader.jsx
+++ src/components/ProvinceHeader.jsx
@@ -2,16 +2,18 @@ import { Carousel } from 'react-bootstrap';
 
 export default function ProvinceHeader() {
   return (
-    <Carousel>
-      {Array.from({ length: 10 }, (_, i) => i).map((i) => (
-        <Carousel.Item key={`Afghanistan${i + 1}`}>
-          <img
-            src={`./Afghanistan${i + 1}.jpg`}
-            className="img-thumbnail"
-            alt="Afghanistan Lanscape"
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div className="p-3">
+      <Carousel>
+        {Array.from({ length: 10 }, (_, i) => i).map((i) => (
+          <Carousel.Item key={`Afghanistan${i + 1}`}>
+            <img
+              src={`./Afghanistan${i + 1}.jpg`}
+              className="img-thumbnail"
+              alt="Afghanistan Lanscape"
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 }

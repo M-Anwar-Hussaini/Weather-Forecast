3cae6646d8407d7ee51e99882397f88a5803760f
Anwar <anwarhussaini160@gmail.com>
2023-08-01 01:30:23 +0430

    Apply search functionality

--- src/components/ProvinceList.jsx
+++ src/components/ProvinceList.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Province from './Province';
 import { getApiData } from '../redux/slice';
@@ -12,34 +12,48 @@ const linkStyle = {
 };
 
 export default function ProvinceList() {
+  const [result, setResult] = useState('');
   const { provinces, isLoading } = useSelector((store) => store.provinces);
   const dispatch = useDispatch();
+
+  const filtered = provinces.filter((el) => el.name.toLowerCase().includes(result));
+
   useEffect(() => {
     if (provinces.length === 0) dispatch(getApiData());
   }, [dispatch, provinces.length]);
 
-  if (isLoading) {
-    return (
-      <Loading />
-    );
-  }
-
   return (
     <div className="row gx-0 border-top">
-      {provinces.map((all, i) => {
-        const pro = getBasicData(all);
-        return (
-          <Link to={`details/${pro.province}`} key={pro.province} style={linkStyle} className="col-6">
-            <Province
-              icon={pro.icon}
-              province={pro.province}
-              temprature={pro.temprature}
-              className={[1, 2].includes(i % 4) ? 'bgcolor-normal' : 'bgcolor-dark'}
-              description={pro.description}
-            />
-          </Link>
-        );
-      })}
+      <div className="container my-3">
+        <input
+          type="search"
+          className="form-control"
+          id="search"
+          value={result}
+          onChange={(e) => setResult(e.target.value)}
+          placeholder="Search"
+        />
+      </div>
+      {isLoading ? <Loading />
+        : filtered.map((all, i) => {
+          const pro = getBasicData(all);
+          return (
+            <Link
+              to={`details/${pro.province}`}
+              key={pro.province}
+              style={linkStyle}
+              className="col-6"
+            >
+              <Province
+                icon={pro.icon}
+                province={pro.province}
+                temprature={pro.temprature}
+                className={[1, 2].includes(i % 4) ? 'bgcolor-normal' : 'bgcolor-dark'}
+                description={pro.description}
+              />
+            </Link>
+          );
+        })}
     </div>
   );
 }

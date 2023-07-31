3141c50b77623ac54c3b1a019d36a26429e277b8
Anwar <anwarhussaini160@gmail.com>
2023-07-31 09:01:23 +0430

    Add Header for proinve details

--- src/components/ProvinceDetails.jsx
+++ src/components/ProvinceDetails.jsx
@@ -1,34 +1,16 @@
-import Carousel from 'react-bootstrap/Carousel';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import Type from 'prop-types';
+import ProvinceHeader from './ProvinceHeader';
 
 export default function ProvinceDetails() {
   const { provinces } = useSelector((store) => store.provinces);
 
   const { id } = useParams();
   const province = provinces.find((pro) => pro.province === id);
-  console.log(id);
   console.log(province);
   return (
-    <>
-      <h2>Hero</h2>
-    </>
+    <main className="p-3">
+      <ProvinceHeader />
+    </main>
   );
 }
-
-function SlideItem({ image, text }) {
-  return (
-    <Carousel.Item>
-      <img src={image} className="img-fluid" alt={text} />
-      <Carousel.Caption>
-        <h5>{text}</h5>
-      </Carousel.Caption>
-    </Carousel.Item>
-  );
-}
-
-SlideItem.propTypes = {
-  image: Type.string.isRequired,
-  text: Type.string.isRequired,
-};

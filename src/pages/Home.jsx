8dcd8a7db76f3aafe0db1ba019c03202cd0b7290
Anwar <anwarhussaini160@gmail.com>
2023-07-31 10:55:41 +0430

    Set header title dynamically based on its page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import Hero from '../components/Hero';
 import ProvinceList from '../components/ProvinceList';
+import { setHeader } from '../redux/slice';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setHeader('Afghanistan'));
+  }, [dispatch]);
+
   return (
     <>
       <Hero />

535b435bde76131be0668575019d237a6c8ff8e0
Anwar <anwarhussaini160@gmail.com>
2023-07-31 22:24:28 +0430

    Remove extra code from store

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,12 +7,4 @@ const store = configureStore({
   },
 });
 
-export async function printData() {
-  const res = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=kabul&appid=e7e6928cc5d52c53ee37d5011f6f8f82&units=metric',
-  );
-  const data = await res.json();
-  return { ...data };
-}
-
 export default store;

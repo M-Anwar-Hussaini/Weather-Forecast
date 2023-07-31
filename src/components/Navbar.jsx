edfb05e4c221cf8eaac5aedbdc3f4d02d96db7ff
Anwar <anwarhussaini160@gmail.com>
2023-07-31 16:27:08 +0430

    Display current date on the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,27 @@ import { Link } from 'react-router-dom';
 
 export default function MyNavbar() {
   const { header } = useSelector((store) => store.provinces);
+  const getDate = () => {
+    const now = new Date();
+    const monthNames = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December',
+    ];
+    return `${monthNames[now.getMonth()]} ${now.getDate()}, ${now.getFullYear()}`;
+  };
   return (
     <header>
       <nav className="d-flex align-items-center justify-content-between p-2 shadow">
-        <Link to="/">
-          {header === 'Afghanistan' ? (
-            <FaHome fontSize="24px" color="#fff" />
-          ) : (
-            <FaAngleLeft fontSize="24px" color="#fff" />
-          )}
-        </Link>
+        <div className="d-flex gap-2 align-items-center">
+          <Link to="/">
+            {header === 'Afghanistan' ? (
+              <FaHome fontSize="24px" color="#fff" />
+            ) : (
+              <FaAngleLeft fontSize="24px" color="#fff" />
+            )}
+          </Link>
+          <span>{getDate()}</span>
+        </div>
         <h3>{header}</h3>
         <div className="d-flex gap-4">
           <FaMicrophone fontSize="24px" cursor="pointer" />

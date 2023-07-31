8b19e0d7fd74ea91a0978567c61f4093feafba85
Anwar <anwarhussaini160@gmail.com>
2023-07-31 10:53:29 +0430

    Add home icon on the menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import { FaAngleLeft, FaMicrophone, FaGear } from 'react-icons/fa6';
+import {
+  FaAngleLeft, FaMicrophone, FaGear,
+} from 'react-icons/fa6';
+import {
+  FaHome,
+} from 'react-icons/fa';
+
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function MyNavbar() {
+  const { header } = useSelector((store) => store.provinces);
   return (
     <header>
       <nav className="d-flex align-items-center justify-content-between p-2 shadow">
         <Link to="/">
-          <FaAngleLeft fontSize="24px" cursor="pointer" color="#fff" />
+          {header === 'Afghanistan' ? (
+            <FaHome fontSize="24px" color="#fff" />
+          ) : (
+            <FaAngleLeft fontSize="24px" color="#fff" />
+          )}
         </Link>
-        <div><h2>Afghanistan</h2></div>
+        <h3>{header}</h3>
         <div className="d-flex gap-4">
           <FaMicrophone fontSize="24px" cursor="pointer" />
           <FaGear fontSize="24px" cursor="pointer" />
